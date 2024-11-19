195288fac0f360f074e580da2297573db44e4d8a
sandychen014035 <tp6u6g0@gmail.com>
2024-11-19 18:12:27 +0800

    1119.商品

--- js/jsSandyProductBrowsing.js
+++ js/jsSandyProductBrowsing.js
@@ -1,55 +1,18 @@
 
+// // 宣告點選到輪播的templates變數
+// let templatesDIV = '';
 
-// productBrowsing 
-// 宣告三個背景變數
-let bgsvc_1 = "";
-let bgsvc_2 = "";
-let bgsvc_3 = "";
-// 宣告三個換圖輪播變數
-let templatesA = '';
-let templatesB = '';
-let templatesC = '';
 
-// 宣告點選到輪播的templates變數
-let templatesDIV = '';
-
-function getTemplatesDiv() {
-	// 1.先取得操作的標籤div
-	templatesADiv = document.querySelector('.templatesA');
-	templatesBDiv = document.querySelector('.templatesB');
-	templatesCDiv = document.querySelector('.templatesC');
-	bgsvc_1Div = document.querySelector('.bgsvc_1');
-	bgsvc_2Div = document.querySelector('.bgsvc_2');
-	bgsvc_3Div = document.querySelector('.bgsvc_3');
-
-	// 2.再對標籤div操作
-	templatesADiv.style.display = "none";
-	templatesBDiv.style.display = "none";
-	templatesCDiv.style.display = "none";
-	bgsvc_1Div.style.display = "none";
-	bgsvc_2Div.style.display = "none";
-	bgsvc_3Div.style.display = "none";
+// function changeJersey() {
+// 	templatesDIV = $('.templatesA');
 }
 
-function changeJersey() {
-	getTemplatesDiv();
-	templatesDIV = $('.templatesA');
-	templatesADiv.style.display = "flex";
-	bgsvc_1Div.style.display = "flex";
-}
-
-function changeSweat() {
-	getTemplatesDiv();
-	templatesDIV = $('.templatesB');
-	templatesBDiv.style.display = "flex";
-	bgsvc_2Div.style.display = "flex";
-}
-function changeShorts() {
-	getTemplatesDiv();
-	templatesDIV = $('.templatesC');
-	templatesCDiv.style.display = "flex";
-	bgsvc_3Div.style.display = "flex";
-}
+// function changeSweat() {
+// 	templatesDIV = $('.templatesB');
+// }
+// function changeShorts() {
+// 	templatesDIV = $('.templatesC');
+// }
 
 // 以下是按鈕
 let pbTBtnDiv = '';
@@ -68,107 +31,6 @@ function turnoffBtn(pbNum) {
 	getDiv(pbNum);
 	pbTBtnDiv.style.display = "none";
 }
-
-// 下面為換圖輪播
-function slideCarousel(direction) {
-	var templateSize = 369 // technically 192px, but accounting for 16px margin
-	var templates = templatesDIV
-	// create variable for width
-	var templateWidth = 0
-	// add width of each template tile
-	templates.each(function () {
-		templateWidth += $(this).outerWidth(true);
-	});
-	// the amount we want to scroll the templates
-	var paginationSize = templates.parent().width();
-
-	// for sliding right
-	if (direction == "right") {
-
-		// get the current position of the templates
-		var currentTemplatePos = parseInt(templates.css('left'))
-		// the new position of the templates after shifting
-		var newTemplatePos = currentTemplatePos - paginationSize
-
-		// if the arrow is visible
-		if ($('#carousel-right').hasClass('showing')) {
-			// if the templates are at zer state
-			if (currentTemplatePos == 0) {
-				// move them a little farther than pagination so they're not covered by the arrows
-				newTemplatePos = currentTemplatePos - paginationSize
-			}
-			// move templates to the newly defined position
-			templates.css("left", newTemplatePos + "px");
-			// if templates have been moved
-			if (newTemplatePos < 0) {
-				// show the left arrow
-				$('#carousel-left').addClass('showing');
-			}
-			// if we're on the last "page" of templates
-			if (-(newTemplatePos) > (templateWidth - paginationSize)) {
-				// hide the right arrow
-				$('#carousel-right').removeClass('showing');
-			}
-			// if the arrow isn't visible
-		} else {
-			// dont' do anything
-			return
-		}
-	}
-
-	// for sliding left
-	if (direction == "left") {
-
-		var currentTemplatePos = parseInt(templates.css('left'))
-		var newTemplatePos = currentTemplatePos + templates.parent().width();
-
-		// if left arrow is visible
-		if ($('#carousel-left').hasClass('showing')) {
-			// if the template "page" is close to zero state
-			if (currentTemplatePos > -templates.parent().width()) {
-				// set it to zero state
-				templates.css("left", 0 + "px");
-			} else {
-				// otherwise navigate to new position
-				templates.css("left", newTemplatePos + "px");
-			}
-			// if the templates are at zero state
-			if (newTemplatePos >= 0) {
-				// show the right arrow
-				$('#carousel-right').addClass('showing');
-				// hide the left arrow
-				$('#carousel-left').removeClass('showing');
-			}
-			// if not on the last "page" of the templates
-			if (-(newTemplatePos) < (templateWidth - paginationSize)) {
-				// show the right arrow again
-				$('#carousel-right').addClass('showing');
-			}
-			// if left arrow isn't visible
-		} else {
-			// do nothing
-			return
-		}
-	}
-
-};
-
-$(document).ready(function () {
-	$('#carousel-left').click(function () {
-		slideCarousel("left");
-	});
-
-	$('#carousel-right').click(function () {
-		slideCarousel("right");
-	});
-
-	changeJersey();
-});
-
-// 在这里添加初始化代码
-$(document).ready(function () {
-	changeJersey(); // 或 changeSweat(); 根据你的默认显示需求
-});
 
 $(document).ready(function () {
 
---
+++ js_e/PbShape.js
@@ -0,0 +1,42 @@
+function PbShape(props) {
+    // 滑鼠滑入pbShape才會顯示pbTBtn
+    const [isHovered, setIsHovered] = useState(false);
+    // 點選"詳細介紹"時,將商品資訊存在localStorage
+    const productContent=function(){
+        localStorage.setItem("shape",props.shape);
+        localStorage.setItem("price",props.price);
+        localStorage.setItem("introduce",props.introduce);
+        localStorage.setItem("selectedProduct",props.dataNum);
+        window.location.href = './commodity_New.html'; // 跳轉到內頁
+    }
+    return <>
+        <li 
+            className="pbShape"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
+            <div className="frame"></div>
+            <div className="smallBox">
+                <div className="smallBoxIcon collection"></div>
+            </div>
+            <div className="pbshapeimg">
+                <div className={props.svcA}></div>
+                <div className={props.svcB}></div>
+            </div>
+            <div className="pbBescribe">
+                <div className="pbText">
+                    <h4>{props.shape}</h4>
+                    <h5>{props.price}</h5>
+                </div>
+                <div className="pbText">
+                    <p>{props.introduce}</p>
+                    <h5>{props.star}</h5>
+                </div>
+                <div id={props.pbNum} className="pbTBtn" data-product={props.dataNum} style={{ display: isHovered ? 'flex' : 'none' }}
+                    // 為按鈕增加點擊事件 
+                    onClick={ productContent }>
+                    詳細介紹
+                </div>
+            </div>
+        </li>
+    </>
+}
