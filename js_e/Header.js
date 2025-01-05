43c6b01e4d59fbeafe4d1b150cd6dfaaa978c26b
sandychen014035 <tp6u6g0@gmail.com>
2025-01-05 12:53:01 +0800

    0105 整理檔案

--- js_e/Header.js
+++ js_e/Header.js
@@ -81,7 +81,7 @@ function Header({ classname }) {
                                 <li><a href="./productBrowsing.html">商品瀏覽</a></li>
                                 <li><a href="./Customization_process.html">訂購說明</a></li>
                                 <li><a href="./priceplan.html">價格方案</a></li>
-                                <li><a href="./customize-React.html"></a></li>
+                                <li><a href="./priceplan.html">球衣模擬</a></li>
                             </ul>
                             <ul className="bottomIcon inNavbar">
                                 <li><a href=""><img src="./images/icon-fb.png" alt="" /></a></li>
